735b6263e2a4f3851b26bcb6a5cd520b6b5805ba
RozeTarig <rose@MacBook-Pro-Rose.local>
2019-07-25 18:13:24 +0300

    task 9

--- destinations/models.py
+++ destinations/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Destination(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField()
-    pretty_picture = models.ImageField()
+    pretty_picture = models.ImageField(null=True , blank=True)
     activities = models.TextField()
     optimal_visiting_season = models.CharField(max_length=10)
